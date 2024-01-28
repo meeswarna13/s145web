1c496f4f3f9fb7820c25a50b433ba310f1ffa9cb
meeswarna13 <148520337+meeswarna13@users.noreply.github.com>
2024-01-28 19:17:20 +0530

    Add files via upload

---
+++ Webs1/script.js
@@ -0,0 +1,34 @@
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: Arial, sans-serif;
+  }
+  
+  .container {
+    text-align: center;
+  }
+  
+  form {
+    margin-bottom: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+  
+  input,
+  select,
+  button {
+    margin-bottom: 10px;
+    padding: 5px;
+    width: 200px;
+  }
+  
+  #validationResults {
+    font-weight: bold;
+    color: red;
+  }
